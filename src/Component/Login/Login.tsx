9e2ce82e2be7d137ab3775f4886c2f367a1aefa0
Serge <1990sergik27@gmail.com>
2023-05-12 17:21:09 +0300

    redux-form - 75

--- src/Component/Login/Login.tsx
+++ src/Component/Login/Login.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { reduxForm, Field  } from 'redux-form';
 
 export const Login = () => {
+    const onSubmit = (data: any) => {
+        console.log(data)
+    }
+
     return (
         <div>
-            Login
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
         </div>
     );
 };
+const LoginForm = (props: any) => {
+    return (
+        //добавляем onSubmit
+        <form onSubmit={props.onSubmit}>
+            <div><Field placeholder={'login'} name={"login"}  component={'input'}/></div>
+            <div><Field placeholder={'pasword'} name={"password"}  component={'input'}/></div>
+            <div><Field type={"checkbox"} name={"rememberMe"} component={'input'}/> remember me</div>
+            <button>submit</button>
+        </form>
+    )
+}
+
+//оборачиваем компоненту в reduxForm и даем имя форме 'login'
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm);
+
+
 
--- src/redax/redux-store.ts
+++ src/redax/redux-store.ts
@@ -4,7 +4,8 @@ import {dialogsReducer} from "./dialogs-reducer";
 import {sidebarReducer} from "./sidebar-reducer";
 import {usersReducer} from "./users-reducer";
 import {authReducer} from "./auth-reducer";
-import thunkMiddleware from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk';
+import { reducer as formReducer} from 'redux-form'             //импортируем инкапсурированный редюсер и переиминовываем в formReducer
 
 
 let rootReducer = combineReducers({
@@ -13,8 +14,11 @@ let rootReducer = combineReducers({
     sidebar: sidebarReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    form: formReducer                                          //комбайним в стор
 });
 export type AppStateType = ReturnType <typeof rootReducer>;         //типизпция state
 
 //вторым параметром добавляем middleware, без него не заработают thunk
 export let store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+// window.store = store
