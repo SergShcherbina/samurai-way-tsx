28aaf17ef0ab11c7e4922eefa4332dcbc37e21a4
SergShcherbina <1990sergik27@gmail.com>
2023-10-19 08:48:11 +0300

    replaced color spinner

--- src/assets/spinner/Spinner.tsx
+++ src/assets/spinner/Spinner.tsx
@@ -10,23 +10,23 @@ export const Spinner = () => {
       viewBox="0 0 128 128"
       xmlSpace="preserve"
     >
-      <rect x="0" y="0" width="100%" height="100%" fill="#FFFFFF" />
-      <circle cx="64.13" cy="64.13" r="27.63" fill="#75a514" />
+      <rect x="0" y="0" width="100%" height="100%" fill="transparent" />
+      <circle cx="64.13" cy="64.13" r="27.63" fill="#2196f3" />
       <path
         d="M64.13 18.5A45.63 45.63 0 1 1 18.5 64.13 45.63 45.63   0 0 1 64.13 18.5zm0 7.85a37.78 37.78 0 1 1-37.78 37.78 37.78 37.78 0 0 1 37.78-37.78z"
         fillRule="evenodd"
-        fill="#75a514"
+        fill='#2196f3'
       />
       <g>
-        <path d="M95.25 17.4a56.26 56.26 0 0 0-76.8 13.23L12.1 26.2a64 64 0 0 1 87.6-15.17z" fill="#75a514" />
+        <path d="M95.25 17.4a56.26 56.26 0 0 0-76.8 13.23L12.1 26.2a64 64 0 0 1 87.6-15.17z" fill='#2196f3' />
         <path
           d="M95.25 17.4a56.26 56.26 0 0 0-76.8 13.23L12.1 26.2a64 64 0 0 1 87.6-15.17z"
-          fill="#75a514"
+          fill='#2196f3'
           transform="rotate   (120 64 64)"
         />
         <path
           d="M95.25 17.4a56.26 56.26 0 0 0-76.8 13.23L12.1 26.2a64 64 0 0 1 87.6-15.17z"
-          fill="#75a514"
+          fill='#2196f3'
           transform="rotate(240 64 64)"
         />
         <animateTransform
